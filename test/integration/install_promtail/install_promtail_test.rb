1541486af6fb2ce991ad29104c14a4d9b6e464a8
Mendy Baitelman <mendy@baitelman.com>
2020-10-21 17:45:19 -0700

    Update install_promtail_test.rb

--- test/integration/install_promtail/install_promtail_test.rb
+++ test/integration/install_promtail/install_promtail_test.rb
@@ -4,12 +4,11 @@
 # found at https://docs.chef.io/inspec/resources/
 
 describe service('promtail') do
-    it { should be_installed }
-    it { should be_enabled }
-    it { should be_running }
-  end
-  
-  describe http('localhost:9080/metrics', ssl_verify: false, enable_remote_worker: true) do
-    its('status') { should eq 200 }
-  end
-  
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe http('localhost:9080/metrics', ssl_verify: false, enable_remote_worker: true) do
+  its('status') { should eq 200 }
+end
